863a12ac6c383fd98d7aec2a7cc1701b60bebddb
Bice93 <bianca.cerulo@hotmail.it>
2022-06-22 15:27:51 +0200

    Insert the Function

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -9,3 +9,19 @@
 
 const wordUser = prompt(`Inserisci una parola`); //chiedo all'utente una parola
 console.log(wordUser);
+
+wordToReverse (wordUser); //chiamata della funzione
+
+
+
+// *****FUNZIONE*****
+//funzione dove vado a prendere ogni singolo carattere e lo inserisco in una variabile chiamata "invertWord"
+function wordToReverse(insertWord) {
+  let invertWord = ``;
+  let index = insertWord.length - 1;
+  while (index >= 0) {
+    invertWord += insertWord[index];
+    index--; //condizione di uscita
+  };
+  console.log(invertWord)
+};
