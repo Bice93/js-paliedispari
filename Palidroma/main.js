16a0f2389800cd40b3447c06f46b36962e478b0b
Bice93 <bianca.cerulo@hotmail.it>
2022-06-22 14:50:21 +0200

    Ask the User

---
+++ Palidroma/main.js
@@ -0,0 +1,11 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+// creare una costante per chiedere all'utente una parola
+// dobbiamo verificare che la parola sia polindroma.
+//...prendo ogni singolo carattere partendo dalla fine e lo inserisco in una variabile. 
+//verifico se questa parola inversa sia uguale alla parola inserita.
+
+const wordUser = prompt(`Inserisci una parola`); //chiedo all'utente una parola
+console.log(wordUser);
